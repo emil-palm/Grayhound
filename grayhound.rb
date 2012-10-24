8c58ecaac30a171c9a59802cbdf161f1bb5f9f9f
Emil Palm <emil.palm@blocket.se>
2012-10-24 11:19:16 +0200

    Moving files correctly

--- grayhound.rb
+++
@@ -1,9 +0,0 @@
-require 'mechanize'
-require 'json'
-require 'plist'
-
-require 'grayhound/base'
-
-module Grayhound
-	VERSION = "0.0.1"
-end
---
+++ lib/grayhound.rb
@@ -0,0 +1,9 @@
+require 'mechanize'
+require 'json'
+require 'plist'
+
+require 'grayhound/base'
+
+module Grayhound
+	VERSION = "0.0.1"
+end
